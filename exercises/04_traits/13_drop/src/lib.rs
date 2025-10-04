eef7f902de7c6039ea74d7fc8bfd2fbe1f81e892
Shantam Maheshwari <maheshwarishantam@gmail.com>
2025-10-04 23:59:39 +0100

    Day 2: Ticket v2

--- exercises/04_traits/13_drop/src/lib.rs
+++ exercises/04_traits/13_drop/src/lib.rs
@@ -2,6 +2,27 @@
 //  unless a certain operation has been performed on it.
 //  You can see the expected API in the tests below.
 
+struct DropBomb {
+    active: bool,
+}
+
+impl DropBomb {
+    fn new() -> Self {
+        Self { active: true }
+    }
+    fn defuse(&mut self) {
+        self.active = false;
+    }
+}
+
+impl Drop for DropBomb {
+    fn drop(&mut self) {
+        if self.active {
+            panic!("Kaboom!");
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
